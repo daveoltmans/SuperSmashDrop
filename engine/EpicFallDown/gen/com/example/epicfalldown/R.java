672eaf4405b64982b12f5305e6040638b3636168
Aron Uneputty <sagareto@gmail.com>
2015-04-04 14:58:02 +0200

    R.java
    geen idee maar het vraagt om een commit

--- engine/EpicFallDown/gen/com/example/epicfalldown/R.java
+++ engine/EpicFallDown/gen/com/example/epicfalldown/R.java
@@ -1637,7 +1637,7 @@ containing a value of this type.
         public static final int wombat=0x7f02003a;
     }
     public static final class id {
-        public static final int RelativeLayout1=0x7f090044;
+        public static final int RelativeLayout1=0x7f090045;
         public static final int action_bar=0x7f090031;
         public static final int action_bar_activity_content=0x7f090003;
         public static final int action_bar_container=0x7f090030;
@@ -1651,34 +1651,35 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f09002e;
         public static final int action_mode_bar_stub=0x7f09002d;
         public static final int action_mode_close_button=0x7f090020;
-        public static final int action_settings=0x7f090048;
+        public static final int action_settings=0x7f090049;
         public static final int activity_chooser_view_content=0x7f090021;
         public static final int always=0x7f090014;
         public static final int beginning=0x7f09001b;
-        public static final int button1=0x7f090046;
+        public static final int button1=0x7f090047;
         public static final int checkbox=0x7f090029;
         public static final int collapseActionView=0x7f090015;
         public static final int decor_content_parent=0x7f09002f;
         public static final int default_activity_button=0x7f090024;
         public static final int dialog=0x7f090019;
         public static final int disableHome=0x7f09000d;
+        public static final int doodmenumenubutton=0x7f090040;
         public static final int dropdown=0x7f09001a;
         public static final int edit_query=0x7f090033;
         public static final int end=0x7f09001c;
         public static final int expand_activities_button=0x7f090022;
         public static final int expanded_menu=0x7f090028;
-        public static final int game=0x7f090040;
+        public static final int game=0x7f090041;
         public static final int home=0x7f090000;
         public static final int homeAsUp=0x7f09000e;
         public static final int icon=0x7f090026;
         public static final int ifRoom=0x7f090016;
         public static final int image=0x7f090023;
-        public static final int imageView1=0x7f090045;
+        public static final int imageView1=0x7f090046;
         public static final int listMode=0x7f09000a;
         public static final int list_item=0x7f090025;
-        public static final int menuButton=0x7f090042;
+        public static final int menuButton=0x7f090043;
         public static final int middle=0x7f09001d;
-        public static final int modeTextView=0x7f090043;
+        public static final int modeTextView=0x7f090044;
         public static final int never=0x7f090017;
         public static final int none=0x7f09000f;
         public static final int normal=0x7f09000b;
@@ -1686,7 +1687,7 @@ containing a value of this type.
         public static final int progress_horizontal=0x7f090008;
         public static final int radio=0x7f09002b;
         public static final int restartButton=0x7f09003f;
-        public static final int scoreTextView=0x7f090041;
+        public static final int scoreTextView=0x7f090042;
         public static final int search_badge=0x7f090035;
         public static final int search_bar=0x7f090034;
         public static final int search_button=0x7f090036;
@@ -1701,7 +1702,7 @@ containing a value of this type.
         public static final int showCustom=0x7f090010;
         public static final int showHome=0x7f090011;
         public static final int showTitle=0x7f090012;
-        public static final int spinnerMode=0x7f090047;
+        public static final int spinnerMode=0x7f090048;
         public static final int split_action_bar=0x7f090004;
         public static final int submit_area=0x7f09003c;
         public static final int tabMode=0x7f09000c;
@@ -1828,6 +1829,7 @@ containing a value of this type.
         public static final int abc_toolbar_collapse_description=0x7f0a0004;
         public static final int action_settings=0x7f0a0012;
         public static final int app_name=0x7f0a0010;
+        public static final int doodmenumenubutton=0x7f0a0017;
         public static final int hello_world=0x7f0a0011;
         public static final int menu=0x7f0a0016;
         public static final int newgame=0x7f0a0014;
