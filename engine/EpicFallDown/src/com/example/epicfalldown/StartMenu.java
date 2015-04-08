154922eb007b40547216c77eb449d42c39beb6e3
niekbats <niek.bats@home.nl>
2015-04-08 12:15:59 +0200

    Update StartMenu.java

--- engine/EpicFallDown/src/com/example/epicfalldown/StartMenu.java
+++ engine/EpicFallDown/src/com/example/epicfalldown/StartMenu.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import com.epicfalldown.View.ModeSpinnerAdapter;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -12,39 +13,46 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
+import android.app.AlertDialog.Builder;
 
 public class StartMenu extends Activity {
-	
+
 	private Button startknop;
-	
+
 	private ArrayList<String> modes = new ArrayList<String>();
-	
+
 	private static String selectedMode;
 	private Spinner mode;
-	
+
+	private ImageButton uitleg;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.startmenu);
-		
-		//maakt startknop en startknop listener aan moet in de onCreate staan
+
+		// maakt startknop en startknop listener aan moet in de onCreate staan
 		startknop = (Button) findViewById(R.id.button1);
 		startknop.setOnClickListener(new startknopListener());
 		modes.add("Easy");
 		modes.add("Medium");
 		modes.add("Hard");
-		
+
 		Color color = new Color();
-		
+
 		mode = (Spinner) findViewById(R.id.spinnerMode);
 		mode.setBackgroundColor(color.WHITE);
 		mode.setAdapter(new ModeSpinnerAdapter(this, R.layout.mode_item, modes));
+
+		uitleg = (ImageButton) findViewById(R.id.imageButton1);
+		uitleg.setOnClickListener(new uitlegListener());
 	}
-	
+
 	/**
 	 * dit is een button listener van de knop startknop
+	 * 
 	 * @author Niek
 	 *
 	 */
@@ -55,20 +63,44 @@ public class StartMenu extends Activity {
 		 */
 		@Override
 		public void onClick(View v) {
-			selectedMode = ""+mode.getSelectedItem().toString();
-			//start spel door het spel scherm te openen
-			
-			//vergeet niet op Alt + Enter(option + return op een Mac) te drukken voor de imports
-			//MainActivity.this moet worden vervangen door de naam van de mainactivity gevolgd door .this
-			//De DisplayMessageActivity.class moet worden veranderd in de naam van de activity die je wilt opstarten gevolgd door .class bijv. naam.class
-			//DisplayMessageActivity is eigenlijk de naam van de activity die je wilt optarten
-			//de naam van de activity moet een overeenkomende naam met de bijbehorende layout hebben
+			selectedMode = "" + mode.getSelectedItem().toString();
+			// start spel door het spel scherm te openen
+
+			// vergeet niet op Alt + Enter(option + return op een Mac) te
+			// drukken voor de imports
+			// MainActivity.this moet worden vervangen door de naam van de
+			// mainactivity gevolgd door .this
+			// De DisplayMessageActivity.class moet worden veranderd in de naam
+			// van de activity die je wilt opstarten gevolgd door .class bijv.
+			// naam.class
+			// DisplayMessageActivity is eigenlijk de naam van de activity die
+			// je wilt optarten
+			// de naam van de activity moet een overeenkomende naam met de
+			// bijbehorende layout hebben
 			Intent intent = new Intent(StartMenu.this, MainActivity.class);
 			startActivity(intent);
-			}
+		}
 	}
-	
-	//standaard aangemaakt geen aanpassingen aan verricht
+
+	/**
+	 * dit is een listener van uitleg imagebutton
+	 * 
+	 * @author Niek
+	 */
+	private class uitlegListener implements View.OnClickListener {
+
+		@Override
+		public void onClick(View v) {
+			Builder builder = new AlertDialog.Builder(StartMenu.this);
+			AlertDialog dialog = builder.create();
+			dialog.setTitle("Uitleg");
+			dialog.setMessage("de uitleg");
+			dialog.setCancelable(true);
+			dialog.show();
+		}
+	}
+
+	// standaard aangemaakt geen aanpassingen aan verricht
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -87,8 +119,8 @@ public class StartMenu extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
-	public static String getSelectedMode(){
+
+	public static String getSelectedMode() {
 		return selectedMode;
 	}
 }
