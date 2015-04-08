792cefd7b42328019906bfbcc5345d534d19919d
Aron Uneputty <sagareto@gmail.com>
2015-04-09 00:35:58 +0200

    extra update?
    ??

---
+++ engine/EpicFallDown/src/com/epicfalldown/objects/PowerUp.java
@@ -0,0 +1,30 @@
+package com.epicfalldown.objects;
+
+import com.example.epicfalldown.GameBoard;
+import com.example.epicfalldown.GameObject;
+
+public class PowerUp extends GameObject{
+
+	@Override
+	public Type getObjectType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getImageId() {
+		return null;
+	}
+
+	@Override
+	public void onTouched(GameBoard gameBoard) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public PowerType getPowerType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
